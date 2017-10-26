7aaf318b136af920e7dcdefc97c255c1ee7216bd
Nandesh Guru <nandesh@cloudcoreo.com>
2017-10-26 14:58:29 -0700

    Update config.rb

--- services/config.rb
+++ services/config.rb
@@ -17,7 +17,6 @@ end
 
 coreo_agent_rule_runner 'cis-endpoints-agent-rules' do
     action :run
-    rules ${AUDIT_AGENT_RULES_ALERT_LIST}
     profiles ${AUDIT_AGENT_PROFILES_ALERT_LIST}
     filter(${FILTERED_OBJECTS}) if ${FILTERED_OBJECTS}
 end
