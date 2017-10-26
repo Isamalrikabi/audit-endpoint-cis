48fdd298844a1d147815e8f5ad82c9b19edc4776
Nandesh Guru <nandesh@cloudcoreo.com>
2017-10-26 14:48:42 -0700

    Update config.rb

--- services/config.rb
+++ services/config.rb
@@ -15,7 +15,7 @@ coreo_agent_audit_profile 'cis-dil-benchmark' do
     timeout 120
 end
 
-coreo_agent_rule_runner 'agent-rules' do
+coreo_agent_rule_runner 'cis-endpoints-agent-rules' do
     action :run
     rules ${AUDIT_AGENT_RULES_ALERT_LIST}
     profiles ${AUDIT_AGENT_PROFILES_ALERT_LIST}
